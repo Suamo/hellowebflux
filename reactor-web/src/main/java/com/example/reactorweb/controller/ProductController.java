77d6ae89f85058309e864c2bdc5765ffd7cb2f54
Anton Vyshnevskyi <anton_vyshnevskyi@epam.com>
2020-03-02 11:52:38 +0200

    server-sent events

--- reactor-web/src/main/java/com/example/reactorweb/controller/ProductController.java
+++ reactor-web/src/main/java/com/example/reactorweb/controller/ProductController.java
@@ -1,13 +1,17 @@
 package com.example.reactorweb.controller;
 
 import com.example.reactorweb.model.Product;
+import com.example.reactorweb.model.ProductEvent;
 import com.example.reactorweb.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/products")
@@ -59,6 +63,13 @@ public class ProductController {
     @DeleteMapping
     public Mono<Void> deleteAllProducts() {
         return repository.deleteAll();
+    }
+
+    @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ProductEvent> getProductEvents() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(val -> new ProductEvent(val, "Product Event")
+                );
     }
 
 }
